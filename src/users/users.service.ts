eb5deca4764d5f58f50267cb840b2f9fb053e4d9
Dume01 <mishraanirudh777@gmail.com>
2025-12-27 13:27:00 +0530

    Correcting test

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,22 +3,22 @@ import { User } from 'src/models';
 
 @Injectable()
 export class UsersService {
-    private users: User[] = [];
+  private users: User[] = [];
 
-    async createUser(user: User) {
-        const existingUser = this.users.find((u) => u.username === user.username);
-        if (existingUser) {
-            throw new Error('User already exists');
-        }
-        this.users.push(user);
-        return user;
+  async createUser(user: User) {
+    const existingUser = this.users.find((u) => u.username === user.username);
+    if (existingUser) {
+      throw new Error('User already exists');
     }
+    this.users.push(user);
+    return user;
+  }
 
-    async getUserByUsername(username: string) {
-        const user = this.users.find((u) => u.username === username);
-        if (!user) {
-            throw new Error('User not found');
-        }
-        return user;
+  async getUserByUsername(username: string) {
+    const user = this.users.find((u) => u.username === username);
+    if (!user) {
+      throw new Error('User not found');
     }
+    return user;
+  }
 }
